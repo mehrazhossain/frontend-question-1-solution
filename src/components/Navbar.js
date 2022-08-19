b23d584d5f4c2f21e5ff0f4ffcaa288634002cd1
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-08-19 20:38:07 +0600

    Navbar section added

---
+++ src/components/Navbar.js
@@ -0,0 +1,123 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav class="bg-gray-800">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-16">
+          <div class="flex-1 flex items-center justify-center sm:items-stretch ">
+            <div class="flex-shrink-0 flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="156"
+                height="52.233"
+                viewBox="0 0 156 52.233"
+              >
+                <g
+                  id="Group_1496"
+                  data-name="Group 1496"
+                  transform="translate(-880 -3.467)"
+                >
+                  <g
+                    id="Layer_2"
+                    data-name="Layer 2"
+                    transform="translate(880 3.467)"
+                  >
+                    <g id="Layer_1" data-name="Layer 1">
+                      <path
+                        id="Path_3386"
+                        data-name="Path 3386"
+                        d="M47.022,0l-.869,7.019H24.585L23.715,0a50.9,50.9,0,0,0-8.889,3.283l-.471,1.374-3.2,9.365L6.36,28.037h11.6l2.875-14.016H49.9l2.875,14.016h11.6L59.584,14.022l-3.2-9.365L55.917,3.3A50.771,50.771,0,0,0,47.022,0Z"
+                        transform="translate(-5.112 0)"
+                        fill="#4392f1"
+                      />
+                      <path
+                        id="Path_3387"
+                        data-name="Path 3387"
+                        d="M244.038,202.23H232.967l-1.917,12.552h9.022Z"
+                        transform="translate(-185.714 -162.549)"
+                        fill="#fdfffc"
+                      />
+                      <path
+                        id="Path_3388"
+                        data-name="Path 3388"
+                        d="M147.775,155.291l-2.052,3.944-2.052-3.944C143.67,153.843,147.775,153.843,147.775,155.291Z"
+                        transform="translate(-115.479 -123.947)"
+                        fill="#4392f1"
+                      />
+                      <path
+                        id="Path_3389"
+                        data-name="Path 3389"
+                        d="M13.539,125.078a13.539,13.539,0,1,1,13.539-13.539,13.539,13.539,0,0,1-13.539,13.539Z"
+                        transform="translate(0 -78.771)"
+                        fill="#fdfffc"
+                      />
+                      <circle
+                        id="Ellipse_51"
+                        data-name="Ellipse 51"
+                        cx="5.229"
+                        cy="5.229"
+                        r="5.229"
+                        transform="translate(8.302 27.543)"
+                        fill="#18191a"
+                      />
+                      <circle
+                        id="Ellipse_52"
+                        data-name="Ellipse 52"
+                        cx="1.754"
+                        cy="1.754"
+                        r="1.754"
+                        transform="translate(15.342 29.552)"
+                        fill="#fdfffc"
+                      />
+                      <path
+                        id="Path_3390"
+                        data-name="Path 3390"
+                        d="M183.979,125.078a13.539,13.539,0,1,1,13.539-13.539A13.539,13.539,0,0,1,183.979,125.078Z"
+                        transform="translate(-136.997 -78.771)"
+                        fill="#fdfffc"
+                      />
+                      <circle
+                        id="Ellipse_53"
+                        data-name="Ellipse 53"
+                        cx="5.229"
+                        cy="5.229"
+                        r="5.229"
+                        transform="translate(41.753 27.543)"
+                        fill="#18191a"
+                      />
+                      <circle
+                        id="Ellipse_54"
+                        data-name="Ellipse 54"
+                        cx="1.754"
+                        cy="1.754"
+                        r="1.754"
+                        transform="translate(48.784 29.552)"
+                        fill="#fdfffc"
+                      />
+                    </g>
+                  </g>
+                  <text
+                    id="Hootstory"
+                    transform="translate(946 37)"
+                    fill="#fff"
+                    font-size="20"
+                    font-family="SegoeUI-Bold, Segoe UI"
+                    font-weight="700"
+                    letter-spacing="-0.037em"
+                  >
+                    <tspan x="0" y="0">
+                      Hootstory
+                    </tspan>
+                  </text>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
