81d8db3d9f42086df53341485ac913d21b3b88b4
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-08-20 00:41:47 +0600

    Added short story section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
+import Stories from './components/Stories';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Stories />
     </div>
   );
 }
